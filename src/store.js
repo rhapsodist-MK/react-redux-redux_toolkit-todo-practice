9f60c87dd10d07bfbe1067f262429cc905d1ae3c
김민우 <rhapsodist@gimmin-uui-MacBookPro.local>
2020-03-21 04:34:59 +0900

    use configureStore which in redux-toolkit

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux'
-import { createAction, createReducer } from "@reduxjs/toolkit"
+import { createAction, createReducer, configureStore } from "@reduxjs/toolkit"
 
 const addTodo = createAction("Add")
 const deleteTodo = createAction("Delete")
@@ -16,7 +16,7 @@ const reducer = createReducer([], {
   }
 })
 
-const store = createStore(reducer)
+const store = configureStore({reducer})
 
 export const actionCreators = {
   addTodo, deleteTodo
